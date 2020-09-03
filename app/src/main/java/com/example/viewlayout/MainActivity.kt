771c53063afa0ad5e0c1e2973574618109c62e4b
Juan <juansebastianveronjsv@gmail.com>
2020-09-03 20:02:44 +0700

    Tambah Activity

--- app/src/main/java/com/example/viewlayout/MainActivity.kt
+++ app/src/main/java/com/example/viewlayout/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.viewlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("Activity", "MainActivity: onCreate")
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("Activity", "MainActivity: onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.e("Activity", "MainActivity: onResume")
+    }
+
+    override fun onSaveInstanceState(){
+        super.onSaveInstanceState(onState,onPersistentState)
     }
 }
